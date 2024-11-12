ff18a75db8bfbafead087e09795bf202b70473ae
dartaryan <dartaryan@gmail.com>
2024-11-12 21:42:16 +0200

    added components and basic files

---
+++ packup/src/app/modules/pack-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PackListRoutingModule } from './pack-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PackListRoutingModule
+  ]
+})
+export class PackListModule { }
