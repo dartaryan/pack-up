227c48a3ef162b36035c48f0e99f198ac40017c0
dartaryan <dartaryan@gmail.com>
2024-11-12 22:29:54 +0200

    add http calls to client

--- packup/src/app/app.component.ts
+++ packup/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ItemModule} from './modules/item.module';
+import {ItemService} from './services/item.service';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ReactiveFormsModule, FormsModule, ItemModule],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule, FormsModule, ItemModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- packup/src/app/services/item.service.ts
+++ packup/src/app/services/item.service.ts
@@ -17,4 +17,16 @@ export class ItemService {
   public getItem(id: number): Observable<Item> {
     return this.httpClient.get<Item>(`${this.baseUrl}/${id}`);
   }
+
+  public createItem(item: Item): Observable<Item> {
+    return this.httpClient.post<Item>(this.baseUrl, item);
+  }
+
+  public updateItem(item: Item): Observable<Item> {
+    return this.httpClient.put<Item>(`${this.baseUrl}/${item.id}`, item);
+  }
+
+  public deleteItem(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
