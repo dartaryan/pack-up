5989972c9910db913db20711fc48a41e8c6fb9c2
dartaryan <dartaryan@gmail.com>
2024-11-15 13:36:02 +0200

    added delete button

--- packup/src/app/components/home/home.component.ts
+++ packup/src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ItemModule} from '../../modules/item.module';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
@@ -18,8 +18,17 @@ import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 })
 
 export class HomeComponent {
-  private itemService = inject(ItemService);
-  public items$: Observable<Item[]> = this.itemService.getItems();
   protected readonly faEdit = faEdit;
   protected readonly faTrashAlt = faTrashAlt;
+  private itemService = inject(ItemService);
+  public items$: Observable<Item[]> = this.itemService.getItems();
+
+  private router = inject(Router);
+
+  public onDelete(id: number): void {
+    this.itemService.deleteItem(id).subscribe((data) => {
+      this.items$ = this.itemService.getItems()
+    });
+
+  }
 }
