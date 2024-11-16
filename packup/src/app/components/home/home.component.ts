0c7b2a846d38ec6c5642ce868ba20069398f05ff
dartaryan <dartaryan@gmail.com>
2024-11-16 10:04:46 +0200

    added sort

--- packup/src/app/components/home/home.component.ts
+++ packup/src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faEdit, faSearch, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { map, Observable } from 'rxjs';
-import { Item } from '../../models/item';
-import { ItemModule } from '../../modules/item.module';
-import { ItemService } from '../../services/item.service';
+import {AsyncPipe} from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faEdit, faSearch, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {map, Observable} from 'rxjs';
+import {Item} from '../../models/item';
+import {ItemModule} from '../../modules/item.module';
+import {ItemService} from '../../services/item.service';
 
 @Component({
   selector: 'app-home',
@@ -32,6 +32,23 @@ export class HomeComponent {
 
   public filterItems(input: string): void {
     this.items$ = this.itemService.getItems().pipe(map((items) => items.filter((item) => item.name.includes(input) || item.status.includes(input) || item.packlist.includes(input) || item.id.toString().includes(input))));
+  }
+
+  public onSort(headerName: string): void {
+    this.items$ = this.items$.pipe(map((items) => {
+      return [...items].sort((a, b) => {
+        const valueA = a[headerName as keyof Item];
+        const valueB = b[headerName as keyof Item];
+
+        if (typeof valueA === 'string' && typeof valueB === 'string') {
+          return valueA.localeCompare(valueB);
+        } else if (typeof valueA === 'number' && typeof valueB === 'number') {
+          return valueA - valueB;
+        } else {
+          return 0;
+        }
+      });
+    }));
   }
 
 }
